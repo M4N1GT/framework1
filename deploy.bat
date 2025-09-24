4b99200981e38c4d23551a3e5c261ba66ae7b63c
Manitra205 <marriniav@gmail.com>
2025-09-24 03:57:01 -0700

    Ajout du dossier framework

--- deploy.bat
+++ deploy.bat
@@ -4,10 +4,11 @@ REM Script de déploiement Windows pour compiler le framework et préparer le pr
 REM ------------------------------------------------------------------------
 
 REM Définition des chemins (à adapter si besoin)
-set "FRAMEWORK_DIR=D:\itu\L3\S5\framework"
+set "FRAMEWORK_DIR=D:\framework\TP1\framework"
 set "BUILD_DIR=%FRAMEWORK_DIR%\build"
-set "TEST_DIR=D:\apache-tomcat-10.1.28\webapps\testFramework"
+set "TEST_DIR=D:\ProNaina\apache-tomcat-10.1.34\webapps\testFramework"
 set "SERVLET_JAR=%FRAMEWORK_DIR%\jakarta.servlet-api_5.0.0.jar"
+set "WEBXML_FILE=%FRAMEWORK_DIR%\web.xml"
 
 REM Création des dossiers de sortie du framework
 if not exist "%BUILD_DIR%" mkdir "%BUILD_DIR%"
@@ -30,16 +31,24 @@ cd /d "%BUILD_DIR%"
 if exist "framework.jar" del "framework.jar"
 jar cvf "framework.jar" -C "classes" .
 
-REM Copie du JAR dans le projet Test (WEB-INF/lib)
+REM Copie du framework.jar dans le projet Test
 echo Copie du framework.jar dans le projet Test...
 if not exist "%TEST_DIR%\WEB-INF\lib" mkdir "%TEST_DIR%\WEB-INF\lib"
 xcopy "%BUILD_DIR%\framework.jar" "%TEST_DIR%\WEB-INF\lib\" /Y >nul
 
-REM Ne PAS copier les classes compilées du framework dans WEB-INF/classes
+REM Copie du web.xml dans le projet Test
+echo Copie du web.xml dans le projet Test...
+if not exist "%TEST_DIR%\WEB-INF" mkdir "%TEST_DIR%\WEB-INF"
+if exist "%WEBXML_FILE%" (
+    xcopy "%WEBXML_FILE%" "%TEST_DIR%\WEB-INF\" /Y >nul
+) else (
+    echo ATTENTION : web.xml introuvable dans %WEBXML_FILE%
+)
 
-
-REM Démarrage optionnel de Tomcat si un chemin est fourni en paramètre
+REM Démarrage de Tomcat si un chemin est passé en paramètre
 if not "%~1"=="" (
     echo Demarrage de Tomcat...
     call "%~1\bin\startup.bat"
 )
+
+echo Deploiement terminé.
